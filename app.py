d2b5f9381e60086a8c236f06c70e0eac9a1f0130
dougaltoms <dougal.toms@gmail.com>
2022-11-15 16:08:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,10 @@ with st.form("my_form"):
         df['elapsed_time'] = df['elapsed_time']/60
 
         title = df["name"]
-        distance = round(int(df['distance'],2))
-        time_hr = round(int(df['moving_time']/60,2))
+        distance = df['distance']
+        distance = int(distance)
+        time_hr = df['moving_time']
+        time_hr = int(time_hr)
         speed = round(distance/time_hr)
         st.header(title)
         st.subheader('Distance: ',distance)
