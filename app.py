26d3a2d5793cab3d5a5ab321d6b31279ab1b87b8
dougaltoms <dougal.toms@gmail.com>
2022-11-18 16:51:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ st.header('Analyse your Strava activity')
 client_id = st.secrets["client_id"]
 client_secret = st.secrets["client_secret"]
 redirect_uri = r'https://stravastream.streamlit.app/'
+api_url = r'https:www.strava.com/api/v3/'
 
 request_url = f'http://www.strava.com/oauth/authorize?client_id={client_id}' \
                   f'&response_type=code&redirect_uri={redirect_uri}' \
@@ -43,19 +44,13 @@ st.markdown(link, unsafe_allow_html=True)
 
 with st.form("my_form"):
    url = st.text_input("Paste url here:")
-   link = f'[Click here to authorise]({request_url})'
-   st.markdown(link, unsafe_allow_html=True)
-
    # Every form must have a submit button.
    submitted = st.form_submit_button("Get data")
 
    if submitted:
 
-        code = url.split('https://stravastream.streamlit.app/?state=&code=')
-        code = code[1]
+        code = url.split('https://stravastream.streamlit.app/?state=&code=')[1]
         code = code[0:40]
-
-        st.text(code)
 
         tokens = requests.post(url='https://www.strava.com/oauth/token',
                        data={'client_id': client_id,
@@ -64,7 +59,6 @@ with st.form("my_form"):
                              'grant_type': 'authorization_code'})
 
         strava_tokens = tokens.json()
-        st.json(strava_tokens)
 
         ###################
         ## Get User Info ##
@@ -78,24 +72,29 @@ with st.form("my_form"):
 
         st.write("Hello, ", name), st.image(profile_pic, width=250)
 
-        # r = get_request(f'activities/{activity_id}')
+        ######################
+        ## Display Activity ##
+        #####################
 
-        # df = pd.DataFrame.from_dict(r, orient="index")
-        # df=df.transpose()
-        # df=df.loc[:,"name":"sport_type"]
-        # df['distance'] = df['distance']/1000
-        # df['moving_time'] = df['moving_time']/60
-        # df['elapsed_time'] = df['elapsed_time']/60
+        activity=  st.text_input("Paste activity url here:")
+        activity_id = activity.split("https://www.strava.com/activities/")[1]
+        r = requests.get(f"{api_url}activities/{activity_id}")
+        df = pd.DataFrame.from_dict(r, orient="index")
+        df=df.transpose()
+        df=df.loc[:,"name":"sport_type"]
+        df['distance'] = df['distance']/1000
+        df['moving_time'] = df['moving_time']/60
+        df['elapsed_time'] = df['elapsed_time']/60
 
-        # pline = r['map']['polyline']
-        # map_coords = polyline.decode(pline)
-        # map_df = pd.DataFrame(map_coords, columns =['lat', 'lon'])
+        pline = r['map']['polyline']
+        map_coords = polyline.decode(pline)
+        map_df = pd.DataFrame(map_coords, columns =['lat', 'lon'])
 
-        # title = r["name"]
-        # distance = str(round((r['distance']/1000),2))
-        # speed = str(round((r['distance']/1000)/((r['elapsed_time']/60)/60)))
-        # st.header(title)
-        # st.text('Distance: '+ distance)
-        # st.text('Average speed (km/h): '+ speed)
-        # st.map(map_df)
-        # st.dataframe(df)
+        title = r["name"]
+        distance = str(round((r['distance']/1000),2))
+        speed = str(round((r['distance']/1000)/((r['elapsed_time']/60)/60)))
+        st.header(title)
+        st.text('Distance: '+ distance)
+        st.text('Average speed (km/h): '+ speed)
+        st.map(map_df)
+        st.dataframe(df)
