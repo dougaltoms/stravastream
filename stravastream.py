08e7ab4b755205d85cfb0d3cf66d4b016ea077d5
dougaltoms <dougal.toms@gmail.com>
2022-11-21 16:41:51 +0000

    Update stravastream.py

--- stravastream.py
+++ stravastream.py
@@ -23,69 +23,81 @@ st.markdown(link)
 ## Show summary ##
 ##################
 
-if st.button("Get Data"):
+# if st.button("Get Data"):
 
-    code = st.experimental_get_query_params()["code"][0]
+#     code = st.experimental_get_query_params()["code"][0]
 
-    if 'code' not in st.session_state:
-        st.session_state['code'] = code
+#     if 'code' not in st.session_state:
+#         st.session_state['code'] = code
 
-    url = 'https://www.strava.com/oauth/token'
-    r = requests.post(url,  data={'client_id': client_id,
-                            'client_secret': client_secret,
-                            'code': code,
-                            'grant_type': 'authorization_code'})
+#     url = 'https://www.strava.com/oauth/token'
+#     r = requests.post(url,  data={'client_id': client_id,
+#                             'client_secret': client_secret,
+#                             'code': code,
+#                             'grant_type': 'authorization_code'})
 
-    access_token = r.json()['access_token']
-    refresh_token = r.json()['refresh_token']
-    expires_at = r.json()['expires_at']
+#     access_token = r.json()['access_token']
+#     refresh_token = r.json()['refresh_token']
+#     expires_at = r.json()['expires_at']
 
-    if 'access_token' not in st.session_state:
-        st.session_state['access_token'] = access_token
+#     if 'access_token' not in st.session_state:
+#         st.session_state['access_token'] = access_token
 
-    if 'refresh_token' not in st.session_state:
-        st.session_state['refresh_token'] = refresh_token
+#     if 'refresh_token' not in st.session_state:
+#         st.session_state['refresh_token'] = refresh_token
 
-    if 'expires_at' not in st.session_state:
-        st.session_state['expires_at'] = expires_at
+#     if 'expires_at' not in st.session_state:
+#         st.session_state['expires_at'] = expires_at
 
-    firstname = r.json()['athlete']['firstname']
-    lastname = r.json()['athlete']['lastname']
-    fullname = firstname + " " + lastname
+#     firstname = r.json()['athlete']['firstname']
+#     lastname = r.json()['athlete']['lastname']
+#     fullname = firstname + " " + lastname
 
-    st.header(f"Hello, {fullname}")
+#     st.header(f"Hello, {fullname}")
 
-    r = requests.get(f"http://www.strava.com/api/v3/athlete/activities?access_token={access_token}")
+#     r = requests.get(f"http://www.strava.com/api/v3/athlete/activities?access_token={access_token}")
 
-    df = pd.DataFrame(r.json())
-    df_display = df[['name', 'distance', 'moving_time', 'total_elevation_gain','sport_type','id']]
-    df_display = df_display.set_index("name")
+#     df = pd.DataFrame(r.json())
+#     df_display = df[['name', 'distance', 'moving_time', 'total_elevation_gain','sport_type','id']]
+#     df_display = df_display.set_index("name")
 
     
 
-    df_display['distance'] = round(df_display['distance']/1000,2)
-    df_display['total_elevation_gain'] = round(df_display['total_elevation_gain'],2)
-    df['moving_time'] = df['moving_time'].astype('float64') 
-    df['moving_time'] = pd.to_datetime(df["moving_time"], unit='m')
-    df_display['Average Speed (kmh)'] = df_display['distance']/df_display['moving_time']
+#     df_display['distance'] = round(df_display['distance']/1000,2)
+#     df_display['total_elevation_gain'] = round(df_display['total_elevation_gain'],2)
+#     df['moving_time'] = df['moving_time'].astype('float64') 
+#     df['moving_time'] = pd.to_datetime(df["moving_time"], unit='m')
+#     df_display['Average Speed (kmh)'] = df_display['distance']/df_display['moving_time']
 
-    st.dataframe(df_display)
+#     st.dataframe(df_display)
 
     ########################
     ## Display 1 activity ##
     ########################
-    with st.form("activity_form"):
+with st.form("activity_form"):
 
-        url = st.text_input("Paste activity URL")
-        submit = st.form_submit_button("Get activity data")
+    code = st.experimental_get_query_params()["code"][0]
 
-        if submit:
+    url = st.text_input("Paste activity URL")
+    submit = st.form_submit_button("Get activity data")
 
-            activity_id = url.split("https://www.strava.com/activities/")[1]
+    if submit:
 
-            r = requests.get(f"http://www.strava.com/api/v3/activities/{activity_id}?access_token={[st.session_state.access_token]}")
-            map_polyline = r.json()['map']['polyline']
-            map_coords = polyline.decode(map_polyline)
-            map_df = pd.DataFrame(map_coords, columns =['lat', 'lon'])
-            st.map(map_df)
+        url = 'https://www.strava.com/oauth/token'
+        r = requests.post(url,  data={'client_id': client_id,
+                                'client_secret': client_secret,
+                                'code': code,
+                                'grant_type': 'authorization_code'})
+
+        access_token = r.json()['access_token']
+        refresh_token = r.json()['refresh_token']
+        expires_at = r.json()['expires_at']
+
+        activity_id = url.split("https://www.strava.com/activities/")[1]
+
+        r = requests.get(f"http://www.strava.com/api/v3/activities/{activity_id}?access_token={[access_token]}")
+        map_polyline = r.json()['map']['polyline']
+        map_coords = polyline.decode(map_polyline)
+        map_df = pd.DataFrame(map_coords, columns =['lat', 'lon'])
+        st.map(map_df)
 
